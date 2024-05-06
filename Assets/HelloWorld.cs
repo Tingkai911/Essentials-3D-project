563674cda9d5bf1bcfb0d063586adbc0762361bd
tingkai911 <tingkai911@gmail.com>
2024-05-06 21:30:53 +0900

    Set up unity on windows

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Test git commit
     }
 
     // Update is called once per frame
