b8659109f4c7b79448c3233ed701c1f5b16ed4c9
tingkai911 <tingkai911@gmail.com>
2024-05-07 11:47:40 +0900

    add c# scripts to allow the objects to rotate/bounce/change shape

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    public string myMessage;
+
     // Start is called before the first frame update
     void Start()
     {
-        // Test git commit
+        //Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(myMessage);
     }
 }
